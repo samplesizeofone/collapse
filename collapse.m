bd2050f4d469b4d0910ce420153d428b968a9d3d
Tyler Spaeth <samplesizeofone@gmail.com>
2015-07-04 10:46:29 -0700

    Add crease pattern renderer and diamond base crease pattern.

--- collapse.m
+++ collapse.m
@@ -1,5 +1,87 @@
 BeginPackage["collapse`"]
 
+(* Data *)
+creasePatternLibrary =
+    <|{
+        "diamond_base" -> <|{
+            "points" -> {
+                "left_back" -> {-2, 0},
+                "center_back" -> {0, 0},
+                "right_back" -> {2, 0},
+                "left_middle" -> {-2, 1},
+                "center_middle" -> {0, 1},
+                "right_middle" -> {2, 1},
+                "left_forward" -> {-2, 2},
+                "left_half_forward" -> {-1, 2},
+                "right_half_forward" -> {1, 2},
+                "right_forward" -> {2, 2},
+                "left_front" -> {-2, 3},
+                "left_half_front" -> {-1, 3},
+                "center_front" -> {0, 3},
+                "right_half_front" -> {1, 3},
+                "right_front" -> {2, 3}
+            },
+            "polygons" -> {
+                "left_back" -> {
+                    "left_back", "center_back", "center_middle", "left_middle"
+                },
+                "right_back" -> {
+                    "right_back", "center_back", "center_middle", "right_middle"
+                },
+
+                "left_middle" -> {
+                    "left_middle", "center_middle", "left_half_forward",
+                    "left_forward"
+                },
+                "right_middle" -> {
+                    "right_middle", "center_middle", "right_half_forward",
+                    "right_forward"
+                },
+                "left_center" -> {
+                    "center_middle", "center_front", "left_half_forward"
+                },
+                "right_center" -> {
+                    "center_middle", "center_front", "right_half_forward"
+                },
+                "left_front" -> {
+                    "left_front", "left_half_front", "left_half_forward",
+                    "left_forward"
+                },
+                "left_half_front" -> {
+                    "left_half_front", "center_front", "left_half_forward"
+                },
+                "right_half_front" -> {
+                    "right_half_front", "center_front", "right_half_forward"
+                },
+                "right_front" -> {
+                    "right_front", "right_half_front", "right_half_forward",
+                    "right_forward"
+                }
+            },
+            "creases" -> <|{
+                "mountain" -> {
+                    {"center_back", "center_middle"},
+                    {"left_middle", "center_middle"},
+                    {"center_middle", "right_middle"},
+                    {"center_middle", "center_front"},
+                    {"right_half_front", "right_half_forward"},
+                    {"left_half_front", "left_half_forward"}
+                },
+                "valley" -> {
+                    {"center_middle", "left_half_forward"},
+                    {"center_middle", "right_half_forward"},
+                    {"left_forward", "left_half_forward"},
+                    {"right_half_forward", "right_forward"},
+                    {"left_half_forward", "center_front"},
+                    {"center_front", "right_half_forward"}
+                }
+            }|>,
+            "fractals" -> {
+            }
+        }|>
+    }|>
+
+(* Geometry *)
 trilaterate[{r1_, r2_, r3_}, {{x2_}, {x3_, y3_}}] :=
     Module[{d, i, j, x, y, z},
         d = x2;
@@ -55,6 +137,44 @@ calculateTangent[{p1_, p2_, p3_}] :=
 calculateNormal[{p1_, p2_, p3_}] :=
     Normalize[
         Cross[calculateTangent[{p1, p2, p3}], calculateBinormal[{p1, p2, p3}]]
+    ]
+
+convertCreasePatternTo3D[creasePattern_] :=
+    Module[{newCreasePattern},
+        newCreasePattern = creasePattern;
+        newCreasePattern["points"] =
+            creasePattern[["points"]] /. {x_ /; NumberQ[x], y_ /; NumberQ[y]} :>
+                {x, y, 0};
+        newCreasePattern
+    ]
+
+(* Render *)
+renderPolygon[polygon_, points_] :=
+    Polygon[polygon[[2]] /. points]
+
+renderCrease[crease_, type_, points_] :=
+    {
+        If[type == "mountain", Red, Blue],
+        Tube[(crease /. points)]
+    }
+
+renderCreasePattern[creasePattern_] :=
+    Module[{points, polygons, creases},
+        points = creasePattern[["points"]];
+        polygons = creasePattern[["polygons"]];
+        creases = creasePattern[["creases"]];
+
+        {
+            Table[
+                renderPolygon[polygon, points],
+                {polygon, polygons}
+            ],
+            Table[
+                renderCrease[crease, type, points],
+                {type, {"mountain", "valley"}},
+                {crease, creases[[type]]}
+            ]
+        }
     ]
 
 EndPackage[]
